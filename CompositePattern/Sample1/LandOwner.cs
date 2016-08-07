9622e8164e42106264ff881278ad8f40c636f89c
gitbulk <giacatiensinh@hotmail.com>
2016-08-08 03:02:59 +0700

    add composit pattern

---
+++ CompositePattern/Sample1/LandOwner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern.Sample1
+{
+    class LandOwner
+    {
+        private readonly IPainter painter;
+        private readonly int housesCount;
+
+        public LandOwner(IPainter painter, int housesCount)
+        {
+            this.painter = painter;
+            this.housesCount = housesCount;
+        }
+
+        public void ManageHouses()
+        {
+            double daysToPaint = this.painter.EstimateDaysToPaint(this.housesCount);
+            Console.WriteLine("Painting houses for {0} day(s).", daysToPaint );
+        }
+    }
+}
---
+++ CompositePattern/Sample1/PaintingCompany.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern.Sample1
+{
+    class PaintingCompany : IPainter
+    {
+        private readonly IEnumerable<IPainter> painters;
+
+        public PaintingCompany(IEnumerable<IPainter> painters)
+        {
+            this.painters = painters;
+        }
+
+        private double GetVelocity(Painter painter)
+        {
+            return painter.EstimateDaysToPaint(1);
+        }
+
+        private double GetTotalVelocity()
+        {
+            return this.painters.Sum(p => GetVelocity((Painter)p));
+        }
+
+        public double EstimateDaysToPaint(int houses)
+        {
+            return GetTotalVelocity() * houses;
+        }
+    }
+}
