2864f55c86f9ee0a8033f4c167f7b701c7115edb
lasthulk <danchithancongab@gmail.com>
2017-01-12 17:31:52 +0700

    get back

---
+++ MediatorPattern/Sample1/ChatRoom.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern.Sample1
+{
+    class ChatRoom : IMediator
+    {
+        private List<User> users = new List<User>();
+        public List<User> User
+        {
+            get
+            {
+                return users;
+            }
+        }
+
+        public void DistributeMessage(User sender, string message)
+        {
+            foreach (var user in this.users)
+            {
+                if (user != sender)
+                {
+                    user.ReceiveMessage(message);
+                }
+            }
+        }
+
+        public void Register(User user)
+        {
+            this.users.Add(user);
+        }
+    }
+}
