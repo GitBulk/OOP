f3fc39153fab53c0ae5932adc347af18de1316dc
lasthulk <danchithancongab@gmail.com>
2017-02-21 17:05:17 +0700

    add Singleton project

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    sealed class Singleton
+    {
+        private static readonly object LockThis = new object();
+        private static volatile Singleton Instance;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Current()
+        {
+            if (Instance == null)
+            {
+                lock (LockThis)
+                {
+                    if (Instance == null)
+                    {
+                        Instance = new Singleton();
+                    }
+                }
+            }
+            return Instance;
+        }
+    }
+}
