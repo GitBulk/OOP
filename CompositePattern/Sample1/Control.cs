855d15560760cee3ac33a0d063c8532d2db7a09e
lasthulk <danchithancongab@gmail.com>
2017-02-23 11:20:02 +0700

    update sample

---
+++ CompositePattern/Sample1/Control.cs
@@ -0,0 +1,15 @@
+﻿namespace CompositePattern.Sample1
+{
+    abstract class Control
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Render(int level = 0);
+
+        public virtual void AddControl(Control control) { }
+
+        public virtual void RemoveControl(Control control) { }
+    }
+}
---
+++ CompositePattern/Sample1/Textbox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CompositePattern.Sample1
+{
+    class Textbox : Control
+    {
+        // Textbox is leaf
+
+        public override void Render(int level = 0)
+        {
+            Console.WriteLine(new string('-', level) + $"Textbox({ Name })Rendered");
+        }
+    }
+}
