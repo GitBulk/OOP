125535f32b6850f356dd2e081ab38012559bb21b
lasthulk <danchithancongab@gmail.com>
2017-02-23 14:41:25 +0700

    new project

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSA;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Map_Method_With_String()
+        {
+            int expected = 5;
+            int actual = GetInternalCode("C");
+            Assert.IsTrue(expected == actual);
+        }
+
+        private static int GetInternalCode(string externalCode)
+        {
+            return externalCode
+                .Map("A", 0)
+                .Map("B", 2)
+                .Map("C", 5)
+                .Else(-1);
+                
+        }
+    }
+}
